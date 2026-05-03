f8283858fa1b72e212537e06dc910b09942cf326
Abhitandan21 <abhishek662726@gmail.com>
2026-05-03 14:20:04 +0530

    alll

--- Frontend/src/components/AboutSection.tsx
+++ Frontend/src/components/AboutSection.tsx
@@ -54,8 +54,7 @@ export const AboutSection = () => {
               <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-background/80" />
               <div className="absolute inset-0 flex items-center justify-center">
                 {/*<span className="font-display text-[15rem] text-primary/10 select-none">道</span>*/}
-               <img src="/C2.jpeg" alt="Banner" className="h-full w-full object-cover" />
-
+                <img src="../public/c2.jpeg" alt="Banner" />
               </div>
               <div className="absolute bottom-0 left-0 right-0 p-8">
                 {/*<blockquote className="font-body text-lg italic text-foreground/90">
@@ -92,8 +91,11 @@ export const AboutSection = () => {
             <div className="grid grid-cols-2 gap-6">
 
               <div className="bg-card p-6 rounded-lg border border-border text-center">
-                <img src="/aff1.jpeg" alt="Banner" className="h-full w-full object-cover" />
-
+                <img
+                  src="../public/aff1.jpeg"
+                  alt="Affiliation Certificate"
+                  className="w-full h-40 object-cover rounded-md mb-4"
+                />
                 <p className="font-body text-sm text-muted-foreground">
                   Affiliation Certificate
                 </p>
@@ -112,7 +114,7 @@ export const AboutSection = () => {
 
             </div>
           </div>
-
+          
 
         </div>
 
